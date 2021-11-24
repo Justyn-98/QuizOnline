0c76d0fb04571d77910d57ef1fa10b0c9cd035b9
Justyn Chroł <57270554+Justyn-98@users.noreply.github.com>
2021-11-24 11:18:00 +0100

    #19 rebuilding app to xamarin forms (#20)
    * #19 removed old projects
    
    * #19 added new xamarin forms project

---
+++ QuizOnline/QuizOnlineApp/QuizOnlineApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using QuizOnlineApp.Services;
+using QuizOnlineApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuizOnlineApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ QuizOnline/QuizOnlineApp/QuizOnlineApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QuizOnlineApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QuizOnlineApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
