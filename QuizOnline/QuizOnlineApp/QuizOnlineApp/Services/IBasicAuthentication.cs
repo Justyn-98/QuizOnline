00ba6581f621aa35619ac67a7df988a918098941
Justyn Chroł <justyn.chr1@gmail.com>
2021-12-02 13:54:52 +0100

    #3 added google-services file & implemented auth service for adroid app

---
+++ QuizOnline/QuizOnlineApp/QuizOnlineApp/Services/IBasicAuthentication.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace QuizOnlineApp.Services
+{
+    public interface IBasicAuthentication
+    {
+        Task<string> Login(string email, string password);
+        Task<string> Register(string email, string password);
+    }
+}
